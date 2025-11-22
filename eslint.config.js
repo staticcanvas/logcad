fc39c5346185ae137380edaafa426bf2bc41d975
sgkens <sgkens@gmail.com>
2025-11-23 10:13:10 +1100

    🔧 config(eslint): Adding ESlint

---
+++ eslint.config.js
@@ -0,0 +1,75 @@
+import js from '@eslint/js';
+
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: {
+                console: 'readonly',
+                document: 'readonly',
+                window: 'readonly',
+                fetch: 'readonly',
+                Promise: 'readonly',
+                Array: 'readonly',
+                Object: 'readonly',
+                String: 'readonly',
+                Number: 'readonly',
+                Boolean: 'readonly',
+                Date: 'readonly',
+                Error: 'readonly',
+                RegExp: 'readonly',
+                Map: 'readonly',
+                Set: 'readonly',
+                JSON: 'readonly',
+                Math: 'readonly'
+            },
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module'
+            }
+        },
+        rules: {
+            'no-unused-vars': ['warn', { 
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_'
+            }],
+            'no-console': 'off',
+            'no-undef': 'error',
+            'no-redeclare': 'warn',
+            'prefer-const': 'warn',
+            'no-var': 'error',
+            'eqeqeq': ['error', 'always'],
+            'curly': ['error', 'all'],
+            'brace-style': ['error', '1tbs'],
+            'semi': ['error', 'always'],
+            'quotes': ['warn', 'single', { avoidEscape: true }],
+            'comma-dangle': ['warn', 'never'],
+            'indent': ['warn', 4, { SwitchCase: 1 }],
+            'no-trailing-spaces': 'warn',
+            'eol-last': ['warn', 'always']
+        }
+    },
+    {
+        files: ['src/**/*.js'],
+        rules: {
+            // Source-specific rules
+        }
+    },
+    {
+        files: ['**/*.test.js', '**/*.spec.js'],
+        rules: {
+            'no-unused-vars': 'off'
+        }
+    },
+    {
+        ignores: [
+            'node_modules/**',
+            'dist/**',
+            'coverage/**',
+            '*.min.js'
+        ]
+    }
+];
+
