0472925f1e19769fcd2b860267077b4d8d59d076
sgkens <sgkens@gmail.com>
2025-12-11 10:22:46 +1100

    config(vite.config): update vite config
     Notes:
    
      - Add copy for jsr.jsonc for rollup copy

--- vite.config.js
+++ vite.config.js
@@ -166,6 +166,8 @@ export default defineConfig({
         { src: 'src', dest: `dist/${pkg_name_WNNS}` },
         // copy package-lock.json to dist for npm ci installs
         { src: 'package-lock.json', dest: `dist/${pkg_name_WNNS}` },
+        // copy jsr.jsonc to dist for jsr publishing
+        { src: 'jsr.jsonc', dest: `dist/${pkg_name_WNNS}` },
       ],
       hook: 'writeBundle', // Run after build finishes
     }),
